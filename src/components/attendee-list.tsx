f2995b2293592e7451f02ff20c6c6a12724caff0
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-04-05 00:24:07 -0300

    adicionando estado de filtro|busca

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -4,14 +4,23 @@ import { Table } from "./table"
 import { TableHeader } from "./table-header"
 import { TableCell } from "./table-cell"
 import { TableRow } from './table-row'
+import { ChangeEvent, useState } from "react"
+
 export function AttendeeList() {
+
+    const [search, setsearch] = useState('');
+
+    function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+        setsearch(event.target.value);
+    }
+
     return (
         <div className=" flex flex-col gap-4">
             <div className="flex gap-3 items-center">
                 <h1 className="text-2xl font-bold">Participante</h1>
                 <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className="size-4 text-emerald-300"/>
-                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm " type="text" placeholder="Buscar Participante..."  />
+                    <input value={search} onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm " type="text" placeholder="Buscar Participante..."  />
                 </div>
 
             </div>
@@ -32,24 +41,24 @@ export function AttendeeList() {
                     {Array.from({length: 8}).map((_,i)=>{
                         return(
                             <TableRow className="hover:bg-white/10" key={i}>
-                            <TableCell>
-                                <input type="checkbox"  className="size-4 bg-black/20 rounded border-white/10"/>
-                            </TableCell>
-                            <TableCell>
-                                12383
-                            </TableCell>
-                            <TableCell>
-                                <div className="flex flex-col gap-1">
-                                    <span className="font-semibold text-white">Duds Ricarte</span>
-                                    <span>lsuircarite@cccufcgedbbr</span>
-                                </div>
-                            </TableCell>
-                            <TableCell>7 dias atrás</TableCell>
-                            <TableCell>3 dias atrás</TableCell>
-                            <TableCell>
-                                <IconButton transparent><MoreHorizontal className="size-4"/></IconButton>
-                            </TableCell>
-                        </TableRow>
+                                <TableCell>
+                                    <input type="checkbox"  className="size-4 bg-black/20 rounded border-white/10"/>
+                                </TableCell>
+                                <TableCell>
+                                    12383
+                                </TableCell>
+                                <TableCell>
+                                    <div className="flex flex-col gap-1">
+                                        <span className="font-semibold text-white">Duds Ricarte</span>
+                                        <span>lsuircarite@cccufcgedbbr</span>
+                                    </div>
+                                </TableCell>
+                                <TableCell>7 dias atrás</TableCell>
+                                <TableCell>3 dias atrás</TableCell>
+                                <TableCell>
+                                    <IconButton transparent><MoreHorizontal className="size-4"/></IconButton>
+                                </TableCell>
+                            </TableRow>
                         )
                     })}
                 </tbody>
@@ -62,13 +71,12 @@ export function AttendeeList() {
                             <div className="inline-flex items-center gap-8 ">
                                 <span>Página 1 de 23</span>  
                                 <div className="flex gap-1.5">
-                                <IconButton><ChevronsLeft className="size-4"/></IconButton>
-                                <IconButton><ChevronLeft className="size-4"/></IconButton>
-                                <IconButton><ChevronRight className="size-4"/></IconButton>
-                                <IconButton><ChevronsRight className="size-4"/></IconButton>
+                                    <IconButton><ChevronsLeft className="size-4"/></IconButton>
+                                    <IconButton><ChevronLeft className="size-4"/></IconButton>
+                                    <IconButton><ChevronRight className="size-4"/></IconButton>
+                                    <IconButton><ChevronsRight className="size-4"/></IconButton>
                                 </div>
                             </div>
-
                         </TableCell>
                     </TableRow>
                 </tfoot>
