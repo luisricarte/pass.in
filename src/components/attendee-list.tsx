f5e15607289da02808e53b3d326b9b8a534e579d
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-04-06 19:54:40 -0300

    adicionando persistencia da página

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -24,11 +24,19 @@ interface Attendee {
 export function AttendeeList() {
 
     const [search, setsearch] = useState('');
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(()=>{
+        const url = new URL(window.location.toString())
+
+        if ( url.searchParams.has('page')){
+            return Number(url.searchParams.get('page'))
+        }
+        return 1
+    });
+
     const [attendees, setAttendees] = useState<Attendee[]>([]);
     const [totalAttendee, setTotalAttendee ] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
         const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
         url.searchParams.set('pageIndex', String(page-1))
         url.searchParams.set('query', search)
@@ -45,13 +53,20 @@ export function AttendeeList() {
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
         setsearch(event.target.value);
-        setPage(1)
+        //setPage(1)
     }
 
-    function goToNextPage() {page < totalPage && setPage(page+1)}
-    function goToPreviousPage(){page > 1 && setPage(page-1)}
-    function goToFirstPage(){setPage(1)}
-    function goToLastPage(){setPage(totalPage)}
+    function setCurrentPage(page:number) {
+        const url = new URL(window.location.toString());
+        url.searchParams.set('page', String(page));
+        window.history.pushState({},"",url);
+        setPage(page);
+    }
+
+    function goToNextPage() {setCurrentPage(page+1)}
+    function goToPreviousPage(){setCurrentPage(page-1)}
+    function goToFirstPage(){setCurrentPage(1)}
+    function goToLastPage(){setCurrentPage(totalPage)}
 
 
     return (
