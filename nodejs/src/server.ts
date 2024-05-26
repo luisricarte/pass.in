3d0cc064fdb3a31d46c58ea7c3ce98be2f7d0fae
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-26 19:55:22 -0300

    adicionando configuracao do banco de dados

--- nodejs/src/server.ts
+++ nodejs/src/server.ts
@@ -2,6 +2,15 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get('/', ()=>{
+    return 'Hello World!'
+})
+
+app.get('/teste', ()=>{
+    return 'HelloTeste!'
+})
+
+
 app.listen({ port: 8088}).then(()=>{
     console.log("HTTP server running")
 })
